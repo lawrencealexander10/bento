74072eca23b61a53dcd4cc0c8d568f93b761640c
Alex Tenn <connection.l10@gmail.com>
2016-04-25 10:22:30 -0400

    style homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
-  # root to: "homepage#index"
-  root to: "products#index"
+  root to: "homepage#index"
+  # root to: "products#index"
   post "product/:permalink", to: "products#buy"
   get "/products" => 'products#index'
   get "basket", to: "orders#show"
