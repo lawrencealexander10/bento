ec263aa07f85bce425ef584813da5f650b705e32
Alex Tenn <connection.l10@gmail.com>
2016-04-24 22:57:23 -0400

    changed iosapp to accepted and changed name in app.js

--- app/controllers/iosapp_controller.rb
+++ app/controllers/iosapp_controller.rb
@@ -3,7 +3,7 @@ class IosappController < ApplicationController
 	protect_from_forgery with: :exception
 
 	def index
-		@accepted_orders= Shoppe::Order.where.not(accepted_at: nil).map do |u|
+		@accepted_orders= Shoppe::Order.where(status: 'accepted').map do |u|
 	       { :fname => u.first_name, :lname => u.last_name, :order => u.order_items.select("order_id, ordered_item_id, quantity")}
 	    end
     respond_to do |format|
